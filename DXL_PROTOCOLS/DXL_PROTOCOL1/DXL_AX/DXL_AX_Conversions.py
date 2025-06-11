c5bee09fdf36f1c2c914a34829a731b2b5bae6b6
Ana Islas <apislasm@gmail.com>
2025-06-10 19:26:02 -0600

    P1 AX DocStrings

--- DXL_PROTOCOLS/DXL_PROTOCOL1/DXL_AX/DXL_AX_Conversions.py
+++ DXL_PROTOCOLS/DXL_PROTOCOL1/DXL_AX/DXL_AX_Conversions.py
@@ -2,27 +2,134 @@ from math import *
 
 class DXL_AX_P1_CONV:
   def deg2bits(self, deg, offset): # REVISAR
+    """
+    Converts **degrees** to **bits** for Dynamixel
+    AX using Protocol 1
+
+    Parameters
+    ----------
+    float deg:
+        degree value
+    
+    int offset:
+        motor offset in bits (0 deg value)   
+
+    Returns
+    -------
+    int
+        degrees value expressed as bits
+
+    Raises
+    ------
+    None
+    """
     return ceil(min(((deg/0.293255) + offset),1023))
 
 
   def rad2bits(self, rad, offset): # REVISAR
+    """
+    Converts **radians** to **bits** for Dynamixel
+    AX using Protocol 1
+
+    Parameters
+    ----------
+    float rad:
+        radians value
+    
+    int offset:
+        motor offset in bits (0 rad value)   
+
+    Returns
+    -------
+    int
+        radians value expressed as bits
+
+    Raises
+    ------
+    None
+    """
     return ceil(min((rad*(180.0/(0.293255 * pi)) + offset),1023))
 
 
   def bits2deg(self, bits, offset): # REVISAR
+    """
+    Converts **bits** to **degrees** for Dynamixel
+    AX using Protocol 1
+
+    Parameters
+    ----------
+    int bits:
+        bits value
+    
+    int offset:
+        motor offset in bits (0 deg value)   
+
+    Returns
+    -------
+    float
+        bits value expressed as degrees
+
+    Raises
+    ------
+    None
+    """
     return (bits-offset)*0.293255
   
 
   def bits2rad(self, bits, offset): # OK
+    """
+    Converts **bits** to **radians** for Dynamixel
+    AX using Protocol 1
+
+    Parameters
+    ----------
+    int bits:
+        degree value
+    
+    int offset:
+        motor offset in bits (0 deg value)   
+
+    Returns
+    -------
+    float
+        bits value expressed as radians
+
+    Raises
+    ------
+    None
+    """
     return (bits-offset)*((0.293255)*(pi/180.0))
   
 
   def calcSpeedBits(self, x0, xf, t): # OK
-      rad_s = abs((xf-x0)/t)
-      b = rad_s*(1/(0.1047 * 0.111)) #( rad/s->rpm * rpm->bits)
+    """
+    Calculates the motor speed in bits per second
 
-      a = min(b,1023)
-      return ceil(max(1,a))
+    Parameters
+    ----------
+    float x0:
+        starting position in radians
+
+    float xf:
+        end position in radians
+
+    float t:
+        playtime 
+
+    Returns
+    -------
+    int
+        speed in rad/s expressed as bits
+
+    Raises
+    ------
+    None
+    """
+    rad_s = abs((xf-x0)/t)
+    b = rad_s*(1/(0.1047 * 0.111)) #( rad/s->rpm * rpm->bits)
+
+    a = min(b,1023)
+    return ceil(max(1,a))
   
   def calcGradpSeg(self, posInicGrados, posFinGrados, seg): 
     gradPseg = ceil(abs(posFinGrados - posInicGrados)/seg)
--- DXL_PROTOCOLS/DXL_PROTOCOL1/DXL_AX/DXL_AX_Functions.py
+++ DXL_PROTOCOLS/DXL_PROTOCOL1/DXL_AX/DXL_AX_Functions.py
@@ -18,6 +18,25 @@ class DXL_AX_P1(DXL_AX_P1_CONV):
         self.LEN_MOVING_SPEED            = 2
         
     def setTorque(self, torque):
+        """
+        Sets ON/OFF the Dynamixel Torque
+
+        Parameters
+        ----------
+        int torque:
+            must be 1 or 0. 1 for torque ON and 0 for torque OFF.
+        
+        int offset:
+            motor offset in bits (0 deg value)   
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        Number Error
+        """
         self.torque = torque
 
         dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, self.ID, self.ADDR_TORQUE_ENABLE, self.torque)
@@ -30,6 +49,22 @@ class DXL_AX_P1(DXL_AX_P1_CONV):
             else: print(str(self.ID) + " Torque disabled correclty")
 
     def setPosition(self, dxl_goal_position):
+        """
+        Writes the value on to Goal Position RAM table to move the motor
+
+        Parameters
+        ----------
+        int dxl_goal_position:
+            Dynamixel goal position in bits.
+ 
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        None
+        """
         dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, self.ID, self.ADDR_GOAL_POSITION, dxl_goal_position)
         if dxl_comm_result != COMM_SUCCESS:
             print(str(self.ID) + " Set Position %s" % packetHandler.getTxRxResult(dxl_comm_result))
@@ -37,6 +72,21 @@ class DXL_AX_P1(DXL_AX_P1_CONV):
             print(str(self.ID) + " Set Position %s" % packetHandler.getRxPacketError(dxl_error))
 
     def readPosition(self):
+        """
+        Reads the value on the Present Position RAM table
+
+        Parameters
+        ----------
+        None
+ 
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        None
+        """
         self.present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, self.ID, self.ADDR_PRESENT_POSITION)
         if dxl_comm_result != COMM_SUCCESS:
             print(str(self.ID) + " Read Position %s" % packetHandler.getTxRxResult(dxl_comm_result))
@@ -44,10 +94,43 @@ class DXL_AX_P1(DXL_AX_P1_CONV):
             print(str(self.ID) + " Read Position %s" % packetHandler.getRxPacketError(dxl_error))
     
     def getPosition(self):
+        """
+        Reads and returns the dynamixel present position
+
+        Parameters
+        ----------
+        None
+ 
+        Returns
+        -------
+        int 
+            present position in bits
+
+        Raises
+        ------
+        None
+        """
         self.readPosition()
         return self.present_position
     
     def setSpeed(self, dxl_goal_speed):
+        """
+        Writes the value to Present Speed RAM table
+
+        Parameters
+        ----------
+        int dxl_goal_speed:
+            Dynamixel goal speed in bits.
+ 
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        None
+        """
+        
         dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, self.ID, self.ADDR_MOVING_SPEED, dxl_goal_speed)
         if dxl_comm_result != COMM_SUCCESS:
             print(str(self.ID) + " Speed Set %s" % packetHandler.getTxRxResult(dxl_comm_result))
