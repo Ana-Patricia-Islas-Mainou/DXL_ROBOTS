77ecd6d761a9d6c5d521eee412f0ffb87a0a0b8f
Ana Islas <apislasm@gmail.com>
2025-06-10 19:14:03 -0600

    P1 MX docStrings OK

--- DXL_PROTOCOLS/DXL_PROTOCOL1/DXL_MX/DXL_MX_Conversions.py
+++ DXL_PROTOCOLS/DXL_PROTOCOL1/DXL_MX/DXL_MX_Conversions.py
@@ -2,27 +2,134 @@ from math import *
 
 class DXL_MX_P1_CONV:
   def deg2bits(self, deg, offset): # OK
+    """
+    Converts **degrees** to **bits** for Dynamixel
+    MX using Protocol 1
+
+    Parameters
+    ----------
+    float deg:
+        degree value
+    
+    int offset:
+        motor offset in bits (0 deg value)   
+
+    Returns
+    -------
+    int
+        degrees value expressed as bits
+
+    Raises
+    ------
+    None
+    """
     return ceil(min(((deg/0.0879) + offset),4095))
 
 
   def rad2bits(self, rad, offset): # OK
+    """
+    Converts **radians** to **bits** for Dynamixel
+    MX using Protocol 1
+
+    Parameters
+    ----------
+    float rad:
+        radians value
+    
+    int offset:
+        motor offset in bits (0 rad value)   
+
+    Returns
+    -------
+    int
+        radians value expressed as bits
+
+    Raises
+    ------
+    None
+    """
     return ceil(min((rad*(180.0/(0.0879 * pi)) + offset),4095))
 
 
   def bits2deg(self, bits, offset): # OK
+    """
+    Converts **bits** to **degrees** for Dynamixel
+    MX using Protocol 1
+
+    Parameters
+    ----------
+    int bits:
+        bits value
+    
+    int offset:
+        motor offset in bits (0 deg value)   
+
+    Returns
+    -------
+    float
+        bits value expressed as degrees
+
+    Raises
+    ------
+    None
+    """
     return (bits-offset)*0.0879
   
 
   def bits2rad(self, bits, offset): # OK
+    """
+    Converts **bits** to **radians** for Dynamixel
+    MX using Protocol 1
+
+    Parameters
+    ----------
+    int bits:
+        degree value
+    
+    int offset:
+        motor offset in bits (0 deg value)   
+
+    Returns
+    -------
+    float
+        bits value expressed as radians
+
+    Raises
+    ------
+    None
+    """
     return (bits-offset)*((0.0879)*(pi/180.0))
   
 
   def calcSpeedBits(self, x0, xf, t): # OK
-      rad_s = abs((xf-x0)/t)
-      b = rad_s*(1/(0.1047 * 0.114)) #( rad/s->rpm * rpm->bits)
+    """
+    Calculates the motor speed in bits per second
 
-      a = min(b,4096)
-      return ceil(max(1,a))
+    Parameters
+    ----------
+    float x0:
+        starting position in radians
+
+    float xf:
+        end position in radians
+
+    float t:
+        playtime 
+
+    Returns
+    -------
+    int
+        speed in rad/s expressed as bits
+
+    Raises
+    ------
+    None
+    """
+    rad_s = abs((xf-x0)/t)
+    b = rad_s*(1/(0.1047 * 0.114)) #( rad/s->rpm * rpm->bits)
+
+    a = min(b,4096)
+    return ceil(max(1,a))
   
 
   def calcGradpSeg(self, posInicGrados, posFinGrados, seg): 
